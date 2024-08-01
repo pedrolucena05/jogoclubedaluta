9b4fca1523a6d5bb86e8ff506d1ed150f295cb12
Pedro Lucena <44994733+pedrolucena05@users.noreply.github.com>
2024-08-01 04:16:16 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const readline = require('readline-sync');
 
 var vitoria = null;
+
 // Superclasse Personagem
 class Personagem {
     constructor(nome, poderAtaque, vida) {
@@ -9,18 +10,12 @@ class Personagem {
       this.vida = vida;
     }
   
-    atacar() {
-      console.log(`${this.nome} atacou com poder de ataque ${this.poderAtaque}.`);
-    }
-  
     receberDano(dano) {
       this.vida -= dano;
-      console.log(`${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}.`);
+      console.log(`\n${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}.`);
     }
   
-    mostrarStatus() {
-      console.log(`Nome: ${this.nome}, Poder de Ataque: ${this.poderAtaque}, Vida: ${this.vida}`);
-    }
+    
 }
   
   // Classe derivada Principal
@@ -29,7 +24,8 @@ class Personagem {
       super(nome, poderAtaque, vida);
       this.superAtaque = superAtaque;
     }
-  
+    
+    //Acabou que não implementei essa funcionalidade no jogo, fica pra proxima atualização. 
     usarSuperAtaque() {
       console.log(`${this.nome} usou Super Ataque com poder de ${this.superAtaque}!`);
     }
@@ -41,15 +37,80 @@ class Personagem {
       super(nome, poderAtaque, vida);
       this.superDefesa = superDefesa;
     }
-  
+
+    //Acabou que não implementei essa funcionalidade no jogo, fica pra proxima atualização.
     usarSuperDefesa() {
       console.log(`${this.nome} usou Super Defesa com poder de ${this.superDefesa}!`);
     }
   }
-  
 
+// Elaboracao das funções
+// Função para mostrar stutus dos lutadores
+function status (protagonista, adversario) {
+  console.log('Status dos lutadores:');
+  console.log(`Você | Ataque: ${protagonista.poderAtaque} | Vida: ${protagonista.vida}`);
+  console.log(`${adversario.nome} | Ataque: ${adversario.poderAtaque} | Vida: ${adversario.vida}`);
+}
+
+// Função de batalha
+function batalha (protagonista, adversario, tempoDeExecucao) {
+  while (true) {
+    console.log(`\nVocê: ${protagonista.vida} `);
+    console.log(`Holyfield: ${adversario.vida}\n`);
+    
+    if (protagonista.vida <= 0 ) {
+      vitoria = false
+      break; // Sai do loop caso algum dos personagens perca a batalha
+    }
+    
+    if ( adversario.vida <= 0 ) {
+      vitoria = true
+      break; // Sai do loop caso algum dos personagens perca a batalha
+    }
+  
+    // Determina a quantidade de caracteres que vai aparecer na tela (entre 1 e 3)
+    let qtd = Math.floor(Math.random() * 3) + 1;
+    
+    var str = ''; 
+    
+    
+    for (let i = 0; i < qtd; i++) {
+      const caractere = String.fromCharCode(Math.floor(Math.random() * 26) + 97); // Gera um caractere aleatório entre 'a' e 'z'
+      str += caractere; //junta o caractere sorteado com a string
+    }
+  
+    //determina o tempo de resposta
+    console.log(str)
+    const inicio = new Date();
+    const data = readline.question('');
+    const fim = new Date();
+  
+    const tempoGasto = fim - inicio; // Calcula o tempo total gasto com a entrada de dados
+    
+    if (data == str && tempoGasto < tempoDeExecucao) { 
+      adversario.receberDano(protagonista.poderAtaque); // Se o tempo gasto for menor que o limite e a sequcia de caracteres estiver correta, o adversário recebe dano de ataque
+    }
+    
+    else if (data == str && tempoGasto >= tempoDeExecucao) {
+      protagonista.receberDano(adversario.poderAtaque); // Se o tempo gasto for maior que o limite o protagonista sofre dano. 
+    }
+  
+    else {
+      protagonista.receberDano(adversario.poderAtaque); // caso a entrada de dados esteja diferente da sorteada, o protagosnista sofre dano
+    }
+  
+  }
+}
+
+function mensagemDerrota(){
+  console.log('\nVocê não é o campeão do Clube da Luta!');
+  console.log('Pressione Enter para continuar...');
+  readline.question('');
+}
+
+// Inicio do script do jogo
 console.log('\nClube da Luta, o jogo');
-console.log('\nVocê esta participando em um campeonato de luta clandestina, no subsolo de um edificio no centro de São Paulo.');
+console.log('\nVocê está participando em um campeonato de luta clandestina, no subsolo de um edificio no centro de São Paulo.');
 console.log('\nPremiação:')
 console.log('1º Lugar: R$5000,00');
 console.log('2º Lugar: R$3000,00');
@@ -58,208 +119,83 @@ console.log('\nInscrição: R$100,00');
 
 console.log('\nPressione Enter para continuar...');
 readline.question('');
-console.log('Você esta tenso esperando a fase final, esta com a pensão atrasada e com risco de ser preso.');
-console.log('As 2 primeiras etapas voce se classificou com folga, ganhando tranquilamente de seus adversarios. Agora esta no quadrangular final (onde restam 4 competidores). ');
-console.log('Para você so resta ser campeão, pois sua divida esta em R$4450,00');
+console.log('Você está tenso esperando a fase final, está com a pensão atrasada e com risco de ser preso.');
+console.log('Nas 2 primeiras etapas você se classificou com folga, ganhando tranquilamente de seus adversários. Agora se encontra na fase de preparação para o quadrangular final (onde restam 4 competidores). ');
+console.log('Para você só resta ser campeão, pois sua dívida esta em R$4450,00');
 
 console.log('\nPressione Enter para continuar...');
 readline.question('');
 
-console.log('Status dos lutadores:');
-console.log('Você | Ataque: 50 | Vida: 200 ');
-console.log('Holyfield | Ataque: 40 | Vida: 200');
-
-const protagonista = new Principal('Protagonista', 50, 200, 100);
+const protagonista = new Principal('Você', 50, 200, 100);
 const ad1 = new Adversario('Holyfield', 40, 200, 80);
+
+status(protagonista, ad1);
 
 console.log('\nPressione Enter para continuar...');
 readline.question('');
 
 console.log('Tutorial de combate:');
-console.log('Ira aparecer no console caracteres (as vezes um e as vezes dois ou tres) e voce tem que digitar os caracteres corretos no menor tempo possivel. Se digitar o caractere errado ou demorar mais de 2 segundos ira sofrer dano de ataque adversario.');
+console.log('Irá aparecer no console caracteres (as vezes um e as vezes dois ou três) e você tem que digitar os caracteres corretos no menor tempo possivel. Se digitar o caractere errado ou demorar mais de 2 segundos irá sofrer dano de ataque adversário.');
 
-console.log('\nA luta vai comecar!')
+console.log('\nA luta vai começar!');
 
 console.log('\nPressione Enter para começar...');
 readline.question('');
 
-while (true) {
-  console.log(`\nVoce: ${protagonista.vida} `);
-  console.log(`Holyfield: ${ad1.vida}\n`);
-  
-  if (protagonista.vida <= 0 ) {
-    vitoria = false
-    break; // Sai do loop caso algum dos personagens morra
-  }
-  
-  if ( ad1.vida <= 0 ) {
-    vitoria = true
-    break; // Sai do loop caso algum dos personagens morra
-  }
-
-  // Determina a quantidade de caracteres que vai aparecer na tela (entre 1 e 3)
-  let qtd = Math.floor(Math.random() * 3) + 1;
-  
-  var str = ''; 
-
-  for (let i = 0; i < qtd; i++) {
-    const caractere = String.fromCharCode(Math.floor(Math.random() * 26) + 97); // Gera um caractere aleatório entre 'a' e 'z'
-    str += caractere; // Mostra o caractere na tela
-    //const tempo = Math.random() * 1500 + 1000; // Gera um tempo aleatório entre 1000 e 2500 ms
-    //setTimeout(() => { console.log(' '); }, tempo); // Espera o tempo aleatório para mostrar o próximo caractere
-  }
-
-  console.log(str)
-  const inicio = new Date();
-  const data = readline.question('');
-  const fim = new Date();
-
-  const tempoGasto = fim - inicio; // Calcula a diferença de tempo em milissegundos
-  
-  if (data == str && tempoGasto < 2000) {
-    ad1.receberDano(protagonista.poderAtaque); // Se o tempo gasto for maior que 1500 ms, o adversário recebe dano do ataque do protagonista
-  }
-  
-  else if (data == str && tempoGasto >= 2000) {
-    protagonista.receberDano(ad1.poderAtaque);  
-  }
-
-  else {
-    protagonista.receberDano(ad1.poderAtaque); 
-  }
-
-}
+batalha(protagonista, ad1, 2000); 
 
 console.log('Pressione Enter para continuar...');
 readline.question('');
 
 if (vitoria) {
-  console.log('\nParabéns! voce venceu! A final se aproxima, Maguila nunca perdeu uma partida. Mas voce vai confiante, 3 anos sem perder no Clube da Luta.');
+
+  const ad2 = new Adversario('Maguila', 50, 200, 80);
+  protagonista.vida = 200
+
+  console.log('\nVocê venceu!') 
+  console.log('Pressione Enter para continuar...');
+  readline.question('');
+
+  console.log('Agora a final se aproxima. Maguila nunca perdeu uma partida, Mas você vai confiante, 3 anos sem perder no Clube da Luta.');
 
   console.log('Pressione Enter para continuar...');
   readline.question('');
 
-  console.log('\nA luta vai começar!');
-  console.log('\nStatus dos lutadores:');
-  console.log('Você | Ataque: 50 | Vida: 200 ');
-  console.log('Maguila | Ataque: 50 | Vida: 200');
+  console.log('\nA luta vai começar!\n');
+  status(protagonista, ad2);
 
-  console.log('\nObservacao: Contra o maguila o tempo limite sera de 1.8 segundos.');
+  console.log('\nObservação: Contra o Maguila o tempo limite sera de 1.8 segundos.');
   console.log('\nPreparado?');
 
   console.log('Pressione Enter para começar a partida...');
   readline.question('');  
 
-  const ad2 = new Adversario('Maguila', 50, 200, 80);
-  protagonista.vida = 200
-
-  while (true) {
-    console.log(`\nVoce: ${protagonista.vida} `);
-    console.log(`Maguila: ${ad2.vida}\n`);
-    
-    if (protagonista.vida <= 0 ) {
-      vitoria = false
-      break; // Sai do loop caso algum dos personagens morra
-    }
-    
-    if ( ad2.vida <= 0 ) {
-      vitoria = true
-      break; // Sai do loop caso algum dos personagens morra
-    }
-  
-    // Determina a quantidade de caracteres que vai aparecer na tela (entre 1 e 3)
-    let qtd = Math.floor(Math.random() * 3) + 1;
-    
-    var str = ''; 
-  
-    for (let i = 0; i < qtd; i++) {
-      const caractere = String.fromCharCode(Math.floor(Math.random() * 26) + 97); // Gera um caractere aleatório entre 'a' e 'z'
-      str += caractere; // Mostra o caractere na tela
-      //const tempo = Math.random() * 1500 + 1000; // Gera um tempo aleatório entre 1000 e 2500 ms
-      //setTimeout(() => { console.log(' '); }, tempo); // Espera o tempo aleatório para mostrar o próximo caractere
-    }
-  
-    console.log(str)
-    const inicio = new Date();
-    const data = readline.question('');
-    const fim = new Date();
-  
-    const tempoGasto = fim - inicio; // Calcula a diferença de tempo em milissegundos
-    
-    if (data == str && tempoGasto < 1800) {
-      ad2.receberDano(protagonista.poderAtaque); // Se o tempo gasto for maior que 1500 ms, o adversário recebe dano do ataque do protagonista
-    }
-    
-    else if (data == str && tempoGasto >= 1800) {
-      protagonista.receberDano(ad2.poderAtaque);  
-    }
-  
-    else {
-      protagonista.receberDano(ad2.poderAtaque); 
-    }
-  
-  }
+  batalha(protagonista, ad2, 1800);
 
   if (vitoria) {
-    console.log('\nVoce é o campeão do Clube da Luta!');
+    console.log('\nVocê é o campeão do Clube da Luta!');
     console.log('Pressione Enter para continuar...');
     readline.question('');
 
-    console.log('A torcida grita: É campeão! É campeão');
+    console.log('A torcida grita: É campeão! É campeão!');
 
     console.log('Pressione Enter para continuar...');
     readline.question('');
 
-    console.log('Agora voce respira aliviado e com alguns hematomas, pagou a pensao e ainda sobrou uma grana para a cerveja!')
+    console.log('Agora você respira aliviado, repensa seus gastos para não passar por esse aperto novamente!.')
 
-    console.log('\nFim')
+    console.log('\nFim\n')
   }
 
+  // Caso perca a segunda luta
   else {
-    console.log('\nVoce não é o campeão do Clube da Luta!');
-    console.log('Pressione Enter para continuar...');
-    readline.question('');
+    mensagemDerrota()
   }
 
 }
 
+// Caso perca a primeira luta
 else {
-    console.log('\nVoce não é o campeão do Clube da Luta!');
-    console.log('Pressione Enter para continuar...');
-    readline.question('');
+    mensagemDerrota()
 }
-
-/*  
-protagonista.mostrarStatus();
-protagonista.atacar();
-protagonista.usarSuperAtaque();
-  
-ad1.mostrarStatus();
-ad1.atacar();
-ad1.usarSuperDefesa();
-  
-  // Simulação de combate
-protagonista.atacar();
-ad1.receberDano(protagonista.poderAtaque);
-  
-ad1.atacar();
-protagonista.receberDano(ad1.poderAtaque);
-
-// Função para calcular o tempo gasto
-function calcularTempoEntrada() {
-    console.log('Pressione Enter para começar...');
-    readline.question(''); // Aguarda o usuário pressionar Enter para iniciar
-    const inicio = new Date(); // Captura o momento inicial
-  
-    console.log('y');
-    readline.question(''); // Aguarda o usuário digitar algo e pressionar Enter
-    const fim = new Date(); // Captura o momento final
-  
-    const tempoGasto = fim - inicio; // Calcula a diferença de tempo em milissegundos
-    console.log(`Você gastou ${tempoGasto / 1000} segundos para pressionar Enter.`);
-  }
-  
-  calcularTempoEntrada();
-*/
-
   
